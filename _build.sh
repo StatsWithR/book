f4355abbcbd25ae94b01588671ad1f253b58ffd8
Colin Rundel <rundel@gmail.com>
2017-12-07 12:56:49 -0500

    Try again

--- _build.sh
+++ _build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
 
+devtools::install_github("statswithr/statsr@BayesFactor")
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
