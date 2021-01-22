e2c75f26eafcae89b176dd97f54032e656d1ec97
merliseclyde <clyde@duke.edu>
2021-01-22 16:26:36 -0500

    updated book to drop reference to statsr@BayesFactor branch

--- _build.sh
+++ _build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
-devtools::install_github("statswithr/statsr@BayesFactor")
+
+#devtools::install_github("statswithr/statsr@BayesFactor")
+devtools::install_github("statswithr/statsr")
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
